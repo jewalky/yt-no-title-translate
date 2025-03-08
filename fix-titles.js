283b23a2eff31c57502e87f26daed96f90263a56
ZZYZX <zzyzx@virtual>
2025-03-08 17:01:30 +0200

    Revert titles to original when OEmbed request fails (happens when embedding is forbidden for a video)

--- fix-titles.js
+++ fix-titles.js
@@ -1,28 +1,34 @@
 (() => {
     const jobs = {};
 
+	const setJobText = (job, text) => {
+		job.text = text;
+		if (job.elementTitle) {
+			job.elementTitle.textContent = job.text;
+		}
+		if (job.elementMainTitle) {
+			job.elementMainTitle.textContent = job.text;
+		}
+		if (job.elementPageTitle) {
+			job.elementPageTitle.textContent = job.text;
+		}
+	}
+
     const handleJob = async (key) => {
         const job = jobs[key];
         try {
             const rawResult = await fetch(`https://www.youtube.com/oembed?url=${encodeURIComponent(job.url)}&format=json`);
             const jsonResult = await rawResult.json();
             job.status = 'done';
-            job.text = jsonResult.title;
-            if (job.elementTitle) {
-                job.elementTitle.textContent = job.text;
-            }
-            if (job.elementMainTitle) {
-                job.elementMainTitle.textContent = job.text;
-            }
-            if (job.elementPageTitle) {
-                job.elementPageTitle.textContent = job.text;
-            }
+			setJobText(job, jsonResult.title);
         } catch (e) {
-            console.error('Failed fetching OEmbed information about URL', job.url, e);
+            console.error('Failed fetching OEmbed information about URL, title reverted to normal', job.url, e);
+			job.status = 'failed';
+			setJobText(job, job.originalText || '<failed to fetch title>');
         }
     }
 
-    const upsertJob = (key) => {
+    const upsertJob = (key, originalTitle) => {
         if (!jobs[key]) {
             const fakeUrl = `https://youtube.com/watch?v=${key}`;
             jobs[key] = {
@@ -30,7 +36,8 @@
                 url: fakeUrl,
                 elementTitle: null,
                 elementPageTitle: null,
-                elementMainTitle: null
+                elementMainTitle: null,
+				originalText: originalTitle
             };
             handleJob(key);
         }
@@ -48,16 +55,16 @@
         return parsedUrl.searchParams.get('v') || null;
     };
 
-    const upsertJobForUrl = (url) => {
+    const upsertJobForUrl = (url, originalTitle) => {
         const elementKey = extractKeyFromUrl(url);
         if (!elementKey) {
             return null;
         }
-        return upsertJob(elementKey);
+        return upsertJob(elementKey, originalTitle);
     }
 
     const checkVideoMainTitleForUpdated = (element) => {
-        const job = upsertJobForUrl(window.location.href);
+        const job = upsertJobForUrl(window.location.href, element.textContent);
         if (!job) {
             return;
         }
@@ -73,7 +80,7 @@
     };
 
     const checkVideoPageTitleForUpdated = (element) => {
-        const job = upsertJobForUrl(window.location.href);
+        const job = upsertJobForUrl(window.location.href, element.textContent);
         if (!job) {
             return;
         }
@@ -93,7 +100,7 @@
         if (!linkTo) {
             return;
         }
-        const job = upsertJobForUrl(linkTo.href);
+        const job = upsertJobForUrl(linkTo.href, element.textContent);
         if (!job) {
             return;
         }
